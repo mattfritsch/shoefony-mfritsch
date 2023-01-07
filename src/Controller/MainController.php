083d984e57c5b59a468c8a2f9239bbea8f81562f
mattfritsch <mattfritsch98380@gmail.com>
2023-01-07 10:57:22 +0100

    Initial commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class MainController extends AbstractController
+{
+    #[Route('/', name: 'main_homepage', methods: 'GET')]
+    public function homepage(): Response
+    {
+        return $this->render('index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/presentation', name: 'main_presentation', methods: 'GET')]
+    public function presentation() : Response
+    {
+        return $this->render('presentation.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'main_contact', methods: 'GET')]
+    public function contact() : Response
+    {
+        return $this->render('contact.html.twig', [
+            'controller_name' => 'MainController'
+        ]);
+    }
+}
---
+++ src/Controller/StoreController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StoreController extends AbstractController
+{
+    #[Route('/store', name: 'app_store')]
+    public function index(): Response
+    {
+        return $this->render('store/product-list.html.twig', [
+            'controller_name' => 'StoreController',
+        ]);
+    }
+
+    #[Route('/store/product/{id}/details/{slug}', name: 'store_show_product', requirements: ['id' => '\d+'])]
+    public function showProduct(int $id, string $slug, Request $request) : Response
+    {
+        return $this->render('store/product-detail.html.twig', [
+            'controller_name' => 'StoreController',
+            'id' => $id,
+            'slug' => $slug,
+            'ip' => $request->getClientIp(),
+            'url' => $this->generateUrl('store_show_product', [
+                'id' => $id,
+                'slug' => $slug
+            ])
+        ]);
+    }
+}
